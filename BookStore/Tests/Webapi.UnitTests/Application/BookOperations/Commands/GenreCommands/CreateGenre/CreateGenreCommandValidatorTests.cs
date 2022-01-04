604a700944cfe796b4cf2c8a4d8bf164a3d9af12
cabbar.allahverdiyev@gmail.com <cabbar.allahverdiyev@gmail.com>
2022-01-04 15:36:34 +0400

    CreateGenreCommandValidationTest create

---
+++ BookStore/Tests/Webapi.UnitTests/Application/BookOperations/Commands/GenreCommands/CreateGenre/CreateGenreCommandValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreWebApi.Application.GenreOperations.CreateGenre;
+using BookStoreWebApi.DBOperations;
+using FluentAssertions;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSetup;
+using Xunit;
+
+namespace Webapi.UnitTests.Application.BookOperations.Commands.GenreCommands
+{
+    public class CreateGenreCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly IBookStoreDbContext _context;
+        public CreateGenreCommandValidatorTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("asd")]
+        [InlineData("as")]
+        public void WhenInvalidInputsAreGiven_Validator_ShouldBeReturnErrors(string genreName)
+        {
+            //arrange
+            CreateGenreCommand command = new(_context);
+            command.Model = new CreateGenreModel() { Name = genreName };
+
+            CreateGenreCommandValidator validator = new();
+            ValidationResult errors = validator.Validate(command);
+
+            //act & assert
+            errors.Errors.Count.Should().BeGreaterThan(0);
+
+        }
+
+
+    }
+}
